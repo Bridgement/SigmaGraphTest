1543b0df8bcf1200f21c848db7a85cfb1081ed05
Josh Miltz <93593857+josh-bridgement@users.noreply.github.com>
2024-01-23 11:18:44 +0000

    t

--- test.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-
-export default class extends Component {
-  get myTitle() {
-    return "Hello from javascript"
-  }
-}
