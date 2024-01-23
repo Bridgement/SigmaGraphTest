e9f00566ad28a66e5e83206f2e9b4e7ffc5cd160
Josh Miltz <93593857+josh-bridgement@users.noreply.github.com>
2024-01-23 11:45:01 +0000

    a

---
+++ hello6.js
@@ -0,0 +1,35 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+export default class extends Component {
+    @service store;
+    
+    text = 'Hello, world!';
+    
+    get myStringToTemplate() {
+        // this.store.peekAll('collection').forEach(element => {
+        //     console.log(element)
+        // });
+        var collection = this.store.peekAll('collection').find(collection => collection.name === 'DbApplication');
+        return `You\'ve selected {{collection}}.`;
+    }
+
+    get isContextSet() {
+        // This will return true only if there is a record selected for sithCollection, shipCollection and that colorComponent selected value has been set
+        return this.args.component.templatingHelper.isContextSet(
+        this.myStringToTemplate,
+        );
+    }
+
+    get myTitle() {
+        if (!this.isContextSet) {
+        return 'Please select a sith, a ship and a color first';
+        }
+
+        return this.args.component.templatingHelper.injectVariablesIntoStringWithoutValidation(
+        this.myStringToTemplate,
+        { expectedType: 'String' },
+        );
+    }
+}
